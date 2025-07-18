c28f913f9779dbecfceac5a8f458609c7842958e
Jonghyun Park <ppjjhh1027@gmail.com>
2025-07-18 11:14:36 +0900

    complete 05_meetingGpt

--- pages/05_MeetingGPT.py
+++ pages/05_MeetingGPT.py
@@ -1,3 +1,5 @@
+from langchain.storage import LocalFileStore
+from langchain_community.vectorstores import Chroma
 import streamlit as st
 import subprocess
 from pydub import AudioSegment
@@ -8,6 +10,15 @@ import openai
 import os
 from langchain_openai import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
+from langchain.document_loaders import TextLoader, UnstructuredFileLoader
+from langchain.text_splitter import (
+    CharacterTextSplitter,
+    RecursiveCharacterTextSplitter,
+)
+from langchain.schema import StrOutputParser
+from langchain.vectorstores.faiss import FAISS
+from langchain.embeddings import CacheBackedEmbeddings, OpenAIEmbeddings
+
 has_transcript = os.path.exists("./.cache/podcast.txt")
 # .env 파일에서 환경변수 불러오기
 load_dotenv()
@@ -16,9 +27,27 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 llm = ChatOpenAI(
-    temperature= 0.1,
-
+    temperature=0.1,
 )
+splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+    chunk_size=800,
+    chunk_overlab=100,
+)
+
+
+@st.cache_data()
+def embed_file(file_path):
+
+    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
+
+    # 오류: 항상 chapter_one.txt만 로드함. 아래처럼 수정 필요:
+    loader = TextLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitter)
+    embeddings = OpenAIEmbeddings()
+    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
+    vectorstore = Chroma.from_documents(docs, cached_embeddings)
+    retriever = vectorstore.as_retriever()
+    return retriever
 
 
 @st.cache_data()
@@ -105,6 +134,51 @@ with st.sidebar:
             st.write(file.read())
 
     with summary_tab:
+
         start = st.button("Generate summary")
         if start:
-            
+
+            loader = TextLoader("./.cache/transcript.txt")
+
+            docs = loader.load_and_split(text_splitter=splitter)
+            # st.write(docs)
+            first_summary_prompt = ChatPromptTemplate.from_template(
+                """
+                Write a consice summary of the following : "{text}"
+                CONCISE SUMMARY:
+
+                """
+            )
+            first_summary_chain = first_summary_prompt | llm | StrOutputParser()
+
+            summary = first_summary_chain.invoke({"text": docs[0].page_content})
+
+            refine_prompt = ChatPromptTemplate.from_template(
+                """
+                Your job is to produce a final summary.
+                We have provided an existing summary up to a certain point: {existing_summary}
+                We have the opportunity to refine the existing summary (only if needed) with some more context below.
+                ------------
+                {context}
+                ------------
+                Given the new context, refine the original summary.
+                If the context isn't useful, RETURN the original summary.
+                """
+            )
+            refine_chain = refine_prompt | llm | StrOutputParser()
+            with st.status("Summurizing...") as status:
+                for num, doc in enumerate(docs[1:]):
+                    status.update(label=f"Processing document {num+1}/{len(docs)-1}")
+                    summary = refine_chain.invoke(
+                        {
+                            "existing_summary": summary,
+                            "context": doc.page_content,
+                        }
+                    )
+                    st.write(summary)
+            st.write(summary)
+
+    with qa_tab:
+        retriever = embed_file(transcript_path)
+        docs = retriever.invoke("do they talk about marcus aurelius?")
+        st.write(docs)
