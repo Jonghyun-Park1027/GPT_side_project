181fe01625de7d422fea349988fa2932cee66099
Jonghyun Park <ppjjhh1027@gmail.com>
2025-07-18 17:04:21 +0900

    end 6 investGPT and test sql agent

--- pages/06_InvestorGPT.py
+++ pages/06_InvestorGPT.py
@@ -1,3 +1,139 @@
+from langchain_core.messages import SystemMessage
 import streamlit as st
+from langchain_openai import ChatOpenAI
+from typing import Type
+from langchain.tools import BaseTool, Tool
+from langchain.agents import initialize_agent, AgentType
+from pydantic import BaseModel, Field
+from langchain.utilities import DuckDuckGoSearchAPIWrapper
+import os
+import requests
+from dotenv import load_dotenv
 
-st.title("Investing GPT")
+load_dotenv()
+
+llm = ChatOpenAI(
+    temperature=0.1,
+    # model='gpt-4.1-2025-04-14'
+)
+alpha_vantage_api_key = os.environ.get("ALPHA_VANTAGE_API_KEY")
+
+
+class StockMarketSymbolSearchToolArgsSchema(BaseModel):
+    query: str = Field(description="The query you will search for")
+
+
+class StockMarketSymbolSearchTool(BaseTool):
+    name: str = "StockMarketSymbolSearch"
+    description: str = """
+    Use this tool to find the stock market symbol for a company.
+    It takes a query as an argument.
+    Example query : Stock Market Symbol for Apple Company
+
+    """
+    args_schema: Type[StockMarketSymbolSearchToolArgsSchema] = (
+        StockMarketSymbolSearchToolArgsSchema
+    )
+
+    def _run(self, query):
+        ddg = DuckDuckGoSearchAPIWrapper()
+        return ddg.run(query)
+
+
+class CompanyOverviewArgsSchema(BaseModel):
+    symbol: str = Field(description="Stock symbol of the company.Example : AAPL, TSLA")
+
+
+class CompanyOverviewTool(BaseTool):
+    name: str = "CompanyOverview"
+    description: str = """
+    Use this to get an overview of the financials of the company.
+    You should enter a stock symbol.
+    """
+    args_schema: Type[CompanyOverviewArgsSchema] = CompanyOverviewArgsSchema
+
+    def _run(self, symbol):
+        r = requests.get(
+            f"https://www.alphavantage.co/query?function=OVERVIEW&symbol={symbol}&apikey={alpha_vantage_api_key}"
+        )
+        return r.json()
+
+
+class CompanyIncomeStatementTool(BaseTool):
+    name: str = "CompanyIncomeStatement"
+    description: str = """
+    Use this to get an incomeStatement of the company.
+    You should enter a stock symbol.
+    """
+    args_schema: Type[CompanyOverviewArgsSchema] = CompanyOverviewArgsSchema
+
+    def _run(self, symbol):
+        r = requests.get(
+            f"https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol={symbol}&apikey={alpha_vantage_api_key}"
+        )
+        return r.json()["annualReports"]
+
+
+class CompanyStockPerformanceTool(BaseTool):
+    name: str = "CompanyStockPerformance"
+    description: str = """
+    Use this to get an weekly stockPerformance of the company.
+    You should enter a stock symbol.
+    """
+    args_schema: Type[CompanyOverviewArgsSchema] = CompanyOverviewArgsSchema
+
+    def _run(self, symbol):
+        r = requests.get(
+            f"https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol={symbol}&apikey={alpha_vantage_api_key}"
+        )
+        response = list(r.json()["Weekly Time Series"].items())[:200]
+        return response
+
+
+agent = initialize_agent(
+    llm=llm,
+    agent=AgentType.OPENAI_FUNCTIONS,
+    # handle_parsing_error=True,
+    verbose=True,
+    tools=[
+        StockMarketSymbolSearchTool(),
+        CompanyOverviewTool(),
+        CompanyIncomeStatementTool(),
+        CompanyStockPerformanceTool(),
+    ],
+    agent_kwargs={
+        "system_message": SystemMessage(
+            content="""
+            You are a hedge fund manager.
+            
+            You evaluate a company and provide your opinion and reasons why the stock is a buy or not.
+              
+            Consider the performance of a stock, the company overview and the income statement.
+            
+            Be assertive in your judgement and recommend the stock or advise the user against it.
+        """
+        )
+    },
+)
+prompt = "Give me financial information on Tesla's stock, considering it's financials, income statements and stock performance and help me analyze if it's a potential good investment"
+
+
+st.set_page_config(
+    page_title="InvestorGPT",
+    page_icon="💼",
+)
+
+st.markdown(
+    """
+    # InvestorGPT
+            
+    Welcome to InvestorGPT.
+            
+    Write down the name of a company and our Agent will do the research for you.
+"""
+)
+
+company = st.text_input("Write the name of the company you are interested on.")
+if company:
+    result = agent.invoke(company)
+    st.write(result["output"].replace("$", "\$"))
