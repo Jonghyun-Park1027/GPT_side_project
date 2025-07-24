ee63d83789e121aad8530c6577ae9537d7db200a
Jonghyun Park <ppjjhh1027@gmail.com>
2025-07-24 16:53:43 +0900

    end

--- gpt_action.py
+++ gpt_action.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from pydantic import BaseModel, Field
 
 app = FastAPI(
@@ -19,8 +19,12 @@ class Quote(BaseModel):
     description="Upon receiving a GET request this endpoint will return a real quiote said by Nicolacus Maximus himself.",
     response_description="A Quote object that contains the quote said by Nicolacus Maximus and the date when the quote was said.",
     response_model=Quote,
+    # openapi_extra={
+    # "x-openai_isConsequential"= True,
+    # },
 )
-def get_quote():
+def get_quote(request: Request):
+    print(request.headers)
     return {
         "quote": "Life is short so eat it all.",
         "year": 1950,
